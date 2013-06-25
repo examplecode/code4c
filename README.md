092844d26055746b7511abbba5be2f2dab9f7e50
chengkai <chengkai.me@gmail.com>
2013-06-25 18:22:20 +0800

    add pthread demo and select socket demo

---
+++ README.md
@@ -0,0 +1,7 @@
+## socket 相关的一些示例
+
+simple_srv_asy   使用select实现异步socket 的服务端
+	gcc -o simple_srv_asy simple_srv_asy.c
+	
+pthread_hello	最简单的pthread的使用方法
+	gcc -o hello_ptherad hello_ptherad.c 	
---
+++ pthread_hello/hello_ptherad.c
@@ -0,0 +1,48 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_ptherad.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2011/06/24 22时27分06秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS	5
+
+void *PrintHello(void *threadid)
+{
+   long tid;
+   tid = (long)threadid;
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0;t<NUM_THREADS;t++){
+     printf("In main: creating thread %ld\n", t);
+     rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+       }
+     }
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
+}
